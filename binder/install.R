4c38533a3508588b4bc3a84e9905fb39537f76ba
Johannes <johannes.breuer@gesis.org>
2021-01-12 20:13:23 +0100

    create Twitter data notebook

--- binder/install.R
+++ binder/install.R
@@ -3,3 +3,8 @@ install.packages("tidyr", repos = "https://cloud.r-project.org/", dependencies=T
 install.packages("magrittr", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("dplyr", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("ggplot2", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("jsonlite", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("scales", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("stringr", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("tidytext", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("wordcloud", repos = "https://cloud.r-project.org/", dependencies=TRUE)
